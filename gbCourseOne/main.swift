a5680933698d19faf054cc6ab3da60224a1d18e9
EvgeniyKolesnikov <mr.oneofus@gmail.com>
2021-12-09 18:45:29 +0300

    Сделал 5 ДЗ

--- gbCourseOne/main.swift
+++ gbCourseOne/main.swift
@@ -7,5 +7,17 @@
 
 import Foundation
 
-print("Hello, World!")
-
+var sportCarBMW = SportCar(markCar: "BMW", topSpped: 200, color: .black, carTypes: .sportcar, typeOfTransmission: .auto)
+sportCarBMW.isEngineOn = true
+sportCarBMW.startStopEngine()
+var trunkCarMecedes = TrunkCar(markCar: "Mercedes", topSpped: 150, color: .purple, carTypes: .truck, typeOfTransmission: .manual)
+trunkCarMecedes.trunkSpaceLeft = 400
+trunkCarMecedes.cargo = 200
+trunkCarMecedes.cargo = 50
+trunkCarMecedes.cargo = 200
+trunkCarMecedes.doorStae = .open
+trunkCarMecedes.startStopEngine()
+print(trunkCarMecedes.id)
+var trunkCarBMW = TrunkCar(markCar: "BMW", topSpped: 180, color: .red, carTypes: .truck, typeOfTransmission: .variator)
+trunkCarBMW.startStopEngine()
+print(trunkCarBMW.id)
